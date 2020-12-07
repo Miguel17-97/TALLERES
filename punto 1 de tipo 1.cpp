0dc4ba12240a1942826008f96061c7f4117f3dd0
davque <75625366+davque@users.noreply.github.com>
2020-12-07 15:57:41 -0500

    Desarrollando el punto 1 tipo 1, se agregaron algunas funciones

--- punto 1 de tipo 1.cpp
+++ punto 1 de tipo 1.cpp
@@ -12,13 +12,82 @@ class Parqueadero
 	private: 
 		int tope;
 		int limit;
-		string arr[20];
+		string arr[];
+		float hora[];
 	public:
 		void parquear(string, float);
 		float liquidar(float);
 		void sacar();
-		int contarceldas(string)
-		
-		
-		
+		int contarceldas(string);
+		Parqueadero();
+		bool parqueaderovacio();
+		bool parqueaderolleno();
+};
+
+Parqueadero::Parqueadero()
+{
+	limit = 19;
+	tope = -1;
+	arr[limit];
+	hora[limit];
 }
+
+bool Parqueadero::parqueaderovacio()
+{
+	if(tope==-1)
+	{
+		return true;	
+	}
+	else
+	{
+		return false;	
+	}	
+}
+
+bool Parqueadero::parqueaderolleno()
+{
+	if(tope==limit)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void Parqueadero::parquear(string vehiculo, float hora)
+{
+	if(parqueaderolleno==false)
+	{
+		tope++;
+		arr[tope] = vehiculo;
+		hora[tope] = hora;
+		cout << "\a";
+		cout << "¡Vehiculo parqueado correctamente!";
+		system("pause");
+	}
+	else
+	{
+		cout << "\a";
+		cout << "No se puede parquear, no hay celdas :(";
+		system("pause");
+	}
+}
+
+void Parqueadero::sacar(string vehiculo)
+{
+	int i;
+	if(colavacia==false)
+	{
+		for(i=tope; i>=0; i--)
+		{
+			if(arr[i] == vehiculo)
+			{
+				
+			}
+		}
+	}
+}
+
+
