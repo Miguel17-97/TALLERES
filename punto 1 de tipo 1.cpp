d1704902691b5991a1e6066d0592fe3a0691a729
davque <75625366+davque@users.noreply.github.com>
2020-12-07 15:27:13 -0500

    Se agrego el punto 1 de tipo 1

---
+++ punto 1 de tipo 1.cpp
@@ -0,0 +1,24 @@
+//Desarrollo del punto 1 tipo 1
+
+//Librerias
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+class Parqueadero
+{
+	private: 
+		int tope;
+		int limit;
+		string arr[20];
+	public:
+		void parquear(string, float);
+		float liquidar(float);
+		void sacar();
+		int contarceldas(string)
+		
+		
+		
+}
