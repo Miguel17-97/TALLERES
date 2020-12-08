82a45b1c48899c418e3e08180edfbf0f70a36a7b
davque <75625366+davque@users.noreply.github.com>
2020-12-08 11:58:18 -0500

    La funcion sacar, se ha acabado

--- punto 1 de tipo 1.cpp
+++ punto 1 de tipo 1.cpp
@@ -2,8 +2,10 @@
 
 //Librerias
 #include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 using namespace std;
 
@@ -13,11 +15,11 @@ class Parqueadero
 		int tope;
 		int limit;
 		string arr[];
-		float hora[];
+		int hora[];
 	public:
 		void parquear(string, float);
 		float liquidar(float);
-		void sacar();
+		void sacar(string, float);
 		int contarceldas(string);
 		Parqueadero();
 		bool parqueaderovacio();
@@ -58,10 +60,12 @@ bool Parqueadero::parqueaderolleno()
 
 void Parqueadero::parquear(string vehiculo, float hora)
 {
-	if(parqueaderolleno==false)
+	if(parqueaderolleno()==false)
 	{
 		tope++;
+		cout << "Ingrese marca de vehiculo y modelo (sin espacios)"; cin >> vehiculo;
 		arr[tope] = vehiculo;
+		cout << "Ingrese la hora de entrada (hora militar)"; cin >> hora;
 		hora[tope] = hora;
 		cout << "\a";
 		cout << "¡Vehiculo parqueado correctamente!";
@@ -75,18 +79,39 @@ void Parqueadero::parquear(string vehiculo, float hora)
 	}
 }
 
-void Parqueadero::sacar(string vehiculo)
+void Parqueadero::sacar(string vehiculo, float hora)
 {
-	int i;
-	if(colavacia==false)
+	int i, horasal, horafin, cobro, a;
+	if(parqueaderovacio()==false)
 	{
 		for(i=tope; i>=0; i--)
 		{
 			if(arr[i] == vehiculo)
 			{
-				
+				arr[i] = NULL; //aca se asigna un vacio en donde estaba el carro, para que quede vacio
+				cout << "Ingrese la hora de salida de " << vehiculo; cin >> horasal;
+				horafin = hora[i] - horasal;
+				cobro = 0; 
+				a = i; //la a se iguala a i, para que a tenga el valor de i, osea la hora, y se pueda manejar la i abajo
+				//ciclo para calcular el precio del parqueo
+				for(i=hora[a]; i<=horafin; i = i + 1000)
+				{
+					cobro = cobro + 1000;
+				}
+				hora[a] = NULL; 
+				cout << "Lo que debe pagar es " << cobro << " vuelva pronto!";
+			}
+			else
+			{
+				cout << "\a";
+				cout << "Ese vehiculo no ha entrado";
 			}
 		}
+	}
+	else
+	{
+		cout << "\a";
+		cout << "No hay vehiculos, espere clientela"; 
 	}
 }
 
